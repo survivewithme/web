7886854ca22647449cd92d322e6b6ce75e2d7654
Chance Hudson <jchancehud@gmail.com>
2019-06-15 03:51:32 -0500

    Add shared button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Colors from '../Colors'
+
+export default class Button extends React.Component<{
+  onClick?: () => void
+}> {
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: Colors.green,
+          borderRadius: 4,
+          padding: 4,
+          color: Colors.offWhite,
+          cursor: 'pointer',
+        }}
+        onClick={this.props.onClick}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
+}
